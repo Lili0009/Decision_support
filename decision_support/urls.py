79a78a55d1adc1d7157f3bba08e4c5cbc004e040
Lian Gaile <149053015+Lili0009@users.noreply.github.com>
2024-10-29 10:06:23 +0800

    views.py minified

--- decision_support/urls.py
+++ decision_support/urls.py
@@ -6,9 +6,9 @@ admin.site.site_title = 'Hydrocision'
 admin.site.index_title = 'Hydrocision Site'
 
 urlpatterns = [
-    path('', views.Dashboard, name='dashboard'),
-    path('forecast/', views.Forecast, name='forecast'),
-    path('business_zone/', views.Business_zone, name='business_zone'),
-    path('img_map/', views.Img_map, name='img_map'),
-    path('get-current-datetime/', views.Get_current_datetime, name='get_current_datetime'),
+    path('', views.Ab, name='dashboard'),
+    path('forecast/', views.Ac, name='forecast'),
+    path('business_zone/', views.Ad, name='business_zone'),
+    path('img_map/', views.Ae, name='img_map'),
+    path('get-current-datetime/', views.Af, name='get_current_datetime'),
 ]
