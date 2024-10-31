0644b13c792764b61c3c64a597c3d97891707e8e
Lian Gaile <149053015+Lili0009@users.noreply.github.com>
2024-10-31 16:57:11 +0800

    water_loss image

--- decision_support/urls.py
+++ decision_support/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
+from django.shortcuts import redirect
 admin.site.site_header = 'Hydrocision Administration'
 admin.site.site_title = 'Hydrocision'
 admin.site.index_title = 'Hydrocision Site'
 
 urlpatterns = [
-    path('', views.Ab, name='dashboard'),
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('admin/') if request.user.is_authenticated else redirect('admin/login/?next=/admin/')),
+    path('dashboard', views.Ab, name='dashboard'),
     path('forecast/', views.Ac, name='forecast'),
     path('business_zone/', views.Ad, name='business_zone'),
     path('img_map/', views.Ae, name='img_map'),
